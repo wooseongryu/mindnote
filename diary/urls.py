0ca2fd3919b49784dc67d3e38653c62beae8a55f
us3754 <useong3754@gmail.com>
2022-09-18 17:09:41 +0900

    URL구조 생성

---
+++ diary/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # path('diary/', views.page_list),
+    # path('diary/info', views.info),
+    # path('diary/write', views.page_create),
+    # path('diary/page/<int:page_id>/', views.page_detail),
+    # path('diary/page/<int:page_id>/edit', views.page_update),
+    # path('diary/page/<int:page_id>/delete', views.page_delete),
+]
