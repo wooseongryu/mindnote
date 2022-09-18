3e9e26fcc2cf31a5b8661f44f0e3b5c0c4e3c50c
us3754 <useong3754@gmail.com>
2022-09-18 23:00:07 +0900

    상세페이지 추가 views.py에 page_detail함수 작성 page_detail함수는 page_detail.html템플릿을 렌더링해서 돌려준다 page_detail.html생성 page_list.html에서 각각의 Page를 눌렀을 때 상세 페이지로 이동하도록 수정

--- diary/views.py
+++ diary/views.py
@@ -6,3 +6,10 @@ def page_list(request):
     object_list = Page.objects.all()
     context["object_list"] = object_list
     return render(request, 'diary/page_list.html', context)
+
+
+def page_detail(request, page_id):
+    context = dict()
+    object = Page.objects.get(id=page_id)
+    context["object"] = object
+    return render(request, 'diary/page_detail.html', context)
