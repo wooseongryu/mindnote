b71e1989a9a93ab10a4089c514742e5818a312a4
us3754 <useong3754@gmail.com>
2022-09-25 15:30:16 +0900

    업데이트 뷰 views.py에 PageUpdateView뷰 추가 urls.py에 PageUpdateView를 사용하도록 수정

--- diary/views.py
+++ diary/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.core.paginator import Paginator
-from django.views.generic import CreateView, ListView, DetailView
+from django.views.generic import CreateView, ListView, DetailView, UpdateView
 from django.urls import reverse
 from diary.models import Page
 from .forms import PageForm
@@ -13,10 +13,6 @@ class PageListView(ListView):
     paginate_by = 8
     page_kwarg = 'page'
 
-
-# def page_detail(request, page_id):
-#     object = Page.objects.get(id=page_id)
-#     return render(request, 'diary/page_detail.html', {'object': object})
 
 class PageDetailView(DetailView):
     model = Page
@@ -37,18 +33,14 @@ class PageCreateView(CreateView):
         return reverse('page-detail', kwargs={'page_id': self.object.id})
 
 
-def page_update(request, page_id):
-    object = Page.objects.get(id=page_id)
-    if request.method == 'POST':
-        form = PageForm(request.POST, instance=object) 
-        # instance로 기존에 존재하는 데이터를 재사용 
-        # instance가 없으면 새로운 포스트가 만들어짐
-        if form.is_valid():
-            form.save()
-            return redirect('page-detail', page_id=object.id)
-    else:
-        form = PageForm(instance=object) # instance로 기존에 존재하는 데이터를 사용한다.
-    return render(request, 'diary/page_form.html', {'form': form})
+class PageUpdateView(UpdateView):
+    model = Page
+    form_class = PageForm
+    template_name = 'diary/page_form.html'
+    pk_url_kwarg = 'page_id'
+
+    def get_success_url(self):
+        return reverse('page-detail', kwargs={'page_id': self.object.id})
 
 
 def page_delete(request, page_id):
